1512913678fc2285a796646e17572cf9e0ff6c9f
toastnjamz <veritydavis@gmail.com>
2019-07-22 22:28:06 -0700

    Updated getAverageValue() method in Cart.java

--- src/main/java/com/openclassrooms/shop/domain/Cart.java
+++ src/main/java/com/openclassrooms/shop/domain/Cart.java
@@ -24,8 +24,11 @@ public class Cart {
      * @param quantity the quantity
      */
     public void addItem(Product product, int quantity) {
-        // Search the cartLineList for the product and if found, increment the quantity. If not found, add to cartLineList.
-		final Predicate<CartLine> filterByProductId = cl -> cl.getProduct().getId().longValue() == product.getId().longValue();
+        // Creates a predicate filter to search for the passed in product by productId
+    	// Searches for the first instance of the predicate, otherwise returns null
+    	// If the predicate is null or greater than 0, add the new item to cartLineList
+    	// Otherwise, if product already exists in the cartLineList, increment quantity by the value passed in
+    	final Predicate<CartLine> filterByProductId = cl -> cl.getProduct().getId().longValue() == product.getId().longValue();
 		final CartLine cartLine = getCartLineList().stream().filter(filterByProductId).findFirst().orElse(null);
 		if (cartLine == null && quantity > 0)
 			getCartLineList().add(new CartLine(product, quantity));
@@ -60,16 +63,18 @@ public class Cart {
     public double getAverageValue()
     {
         // If the list isn't empty, for each product in the cartLineList, 
-    	// get the subtotal and add it to the totalValue variable.
-    	// Otherwise return the initial value of the averageValue variable.
-    	double averageValue = 0.0;
+    	// get the subtotal and add it to the subTotal variable.
+    	// Otherwise return the initial value of the subTotal variable.
+    	double subTotal = 0.0;
+    	int quantity = 0;
     	if (!getCartLineList().isEmpty()) {
-	    	for (CartLine item : getCartLineList()) {
-	    		averageValue += item.getSubtotal();
-	         }
-	        return averageValue / getCartLineList().size();
+	    	for (CartLine cl : getCartLineList()) {
+	    		subTotal += cl.getSubtotal();
+	    		quantity += cl.getQuantity();
+	    	}
+	    	return subTotal / quantity;
     	}
-    	return averageValue;
+    	return subTotal;
     }
 
     /**
